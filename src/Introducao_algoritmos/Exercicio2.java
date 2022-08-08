a94e2f3c3dcbcd76caf7d8e37b23b611cce88483
unknown <ankiflavio@gmail.com>
2022-08-08 13:02:58 -0300

    Elaboração dos cálculos do exercício 2

---
+++ src/Introducao_algoritmos/Exercicio2.java
@@ -0,0 +1,23 @@
+package Introducao_algoritmos;
+
+	import java.util.Scanner;
+	public class Exercicio2{
+
+		public static void main(String[] args) {
+		Scanner ler = new Scanner (System.in);
+	
+		int anos, meses, dias, diastotais;
+	
+			System.out.println("Entre com quantos dias de vida você possui até a data de hoje: ");
+				diastotais = ler.nextInt();
+			
+				anos = diastotais/365;
+				meses = 30%anos;
+				dias = (diastotais%365)%30;
+						
+		
+		System.out.println("Parabéns você possui: "+ anos + " Anos " + meses + " Meses e " + dias + " dias!!!");
+
+
+}
+}
